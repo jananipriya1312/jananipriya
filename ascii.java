3bb33815ee979b5c76ccab810644f42ea6297d5a
jananipriya1312 <janani.priyaeee@ksrce.ac.in>
2017-07-04 14:29:48 +0530

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,14 @@
+package ascii;
+
+public class ascii {
+
+	public static void main(String[] args) {
+		int a[]={97,98,99,100};
+		for(int i=0;i<4;i++){
+			char b=(char)a[i];
+System.out.print(" \t"+b);
+		}
+
+	}
+
+}
